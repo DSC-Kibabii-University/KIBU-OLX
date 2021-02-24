579fcc781a2b1508dc5538526a767b3cf4133835
JoelKanyi <joelkanyi98@gmail.com>
2021-02-24 13:46:58 +0300

    planted timber debug tree

--- app/src/main/java/com/ifixhubke/kibu_olx/KibuOlxApplication.java
+++ app/src/main/java/com/ifixhubke/kibu_olx/KibuOlxApplication.java
@@ -2,9 +2,13 @@ package com.ifixhubke.kibu_olx;
 
 import android.app.Application;
 
+import timber.log.Timber;
+
 public class KibuOlxApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Timber.plant(new Timber.DebugTree());
     }
 }
