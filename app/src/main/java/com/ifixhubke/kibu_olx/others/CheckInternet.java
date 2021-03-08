abdf055cf520f55ff7f3590e27b1171d01505f3b
lokalawashington <lokalawashington2019@gmail.com>
2021-03-08 08:31:54 -0500

    worked on last seen

--- app/src/main/java/com/ifixhubke/kibu_olx/others/CheckInternet.java
+++ app/src/main/java/com/ifixhubke/kibu_olx/others/CheckInternet.java
@@ -1,8 +1,17 @@
 package com.ifixhubke.kibu_olx.others;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 import timber.log.Timber;
 
@@ -18,5 +27,30 @@ public class CheckInternet {
         } else {
             return false;
         }
+    }
+    public static void updateUserStatus(String status) {
+        FirebaseAuth mAuth;
+        DatabaseReference rootReF;
+
+        String currentUserId;
+        String saveCurrentTime , saveCurrentDate;
+
+        Calendar calendar = Calendar.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        rootReF = FirebaseDatabase.getInstance().getReference();
+
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd,yyyy");
+        saveCurrentDate = currentDate.format(calendar.getTime());
+
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat currentTime = new SimpleDateFormat("hh:mm a");
+        saveCurrentTime = currentTime.format(calendar.getTime());
+
+        HashMap<String,Object> onlineStateMap = new HashMap<>();
+        onlineStateMap.put("time",saveCurrentTime);
+        onlineStateMap.put("date",saveCurrentDate);
+        onlineStateMap.put("state",status);
+
+        currentUserId = mAuth.getCurrentUser().getUid();
+        rootReF.child("users").child(currentUserId).child("userState").updateChildren(onlineStateMap);
     }
 }
