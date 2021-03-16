a108a9152f5ee2ce97742243ab82704afc222283
robert-muriithi <njuguiinirobert@gmail.com>
2021-03-16 12:31:03 +0300

    still working

--- app/src/main/java/com/ifixhubke/kibu_olx/ui/fragments/sell/SellFragmentTwo.java
+++ app/src/main/java/com/ifixhubke/kibu_olx/ui/fragments/sell/SellFragmentTwo.java
@@ -11,6 +11,7 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -68,6 +69,8 @@ public class SellFragmentTwo extends Fragment {
 
         assert getArguments() != null;
         sellArgs = SellFragmentTwoArgs.fromBundle(getArguments()).getSellTwoArguments();
+
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(requireContext(),)
 
         imagesList = sellArgs.getImagesList();
 
