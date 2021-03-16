97a994ca2f6d1c6051dde6f93c3be74b0b1052cf
JoelKanyi <joelkanyi98@gmail.com>
2021-03-16 07:57:01 +0300

    worked on the popup menu

--- app/src/main/java/com/ifixhubke/kibu_olx/ui/fragments/home/HomeFragment.java
+++ app/src/main/java/com/ifixhubke/kibu_olx/ui/fragments/home/HomeFragment.java
@@ -4,16 +4,20 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,6 +30,7 @@ import com.ifixhubke.kibu_olx.data.Favourites;
 import com.ifixhubke.kibu_olx.data.Item;
 import com.ifixhubke.kibu_olx.databinding.FragmentHomeBinding;
 import com.ifixhubke.kibu_olx.others.ItemClickListener;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -34,51 +39,45 @@ import java.util.UUID;
 
 import timber.log.Timber;
 
-public class HomeFragment extends Fragment implements ItemClickListener {
+//ItemClickListener, MaterialSearchBar.OnSearchActionListener, PopupMenu.OnMenuItemClickListener, Toolbar.OnMenuItemClickListener {
+public class HomeFragment extends Fragment implements ItemClickListener, MaterialSearchBar.OnSearchActionListener,
+        Toolbar.OnMenuItemClickListener, androidx.appcompat.widget.PopupMenu.OnMenuItemClickListener {
+
     FragmentHomeBinding binding;
     private AllItemsAdapter adapter;
     private DatabaseReference databaseReference;
-    ArrayList<Item> itemsList;
+    ArrayList<Item> itemsList = new ArrayList<>();
     FirebaseRecyclerOptions<Item> options;
     ItemClickListener itemClickListener;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Timber.d("onCreate");
-    }
-
-    @Override
-    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        Timber.d("onCreateView");
-
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
-        itemsList =new ArrayList<>();
+
         databaseReference = FirebaseDatabase.getInstance().getReference();
 
-        setHasOptionsMenu(true);
-
+        binding.toolbar.setOnMenuItemClickListener(this);
+        binding.searchBar.setOnSearchActionListener(this);
         binding.searchBar.inflateMenu(R.menu.main_menu);
+        binding.searchBar.getMenu().setOnMenuItemClickListener(this);
+
+        binding.searchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
         initializeRecycler();
-
-        /*binding.searchBar.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                filter(s.toString());
-            }
-        });*/
 
         binding.floatingActionButton.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_homeFragment2_to_sellFragmentOne));
 
@@ -146,21 +145,6 @@ public class HomeFragment extends Fragment implements ItemClickListener {
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        Timber.d("onStart");
-        adapter.startListening();
-    }
-
-
-    @Override
-    public void onStop() {
-        Timber.d("onStop");
-        super.onStop();
-        adapter.stopListening();
-    }
-
-    @Override
     public void onPause() {
         super.onPause();
         Timber.d("onPause");
@@ -175,25 +159,87 @@ public class HomeFragment extends Fragment implements ItemClickListener {
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        Timber.d("onDestroyView");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Timber.d("onDestroy");
-    }
-
-    @Override
     public void addItemToFavorites(Item item, int position) {
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("favoriteitems");
-        databaseReference.child(UUID.randomUUID().toString()).setValue(item).addOnSuccessListener(aVoid -> Toast.makeText(requireContext(), item.getItemName() + " to favorites successfully", Toast.LENGTH_SHORT).show());
+        databaseReference.child(UUID.randomUUID().toString()).setValue(item).addOnSuccessListener(aVoid ->
+                Toast.makeText(requireContext(), item.getItemName() + " to favorites successfully", Toast.LENGTH_SHORT).show());
     }
 
     @Override
     public void clickCard(Favourites favourites, int position) {
-        
+
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if (item.getItemId() == R.id.filter_menu) {
+            Navigation.findNavController(requireView()).navigate(R.id.action_homeFragment2_to_filterFragment);
+            return true;
+        } else if (item.getItemId() == R.id.shareMenu) {
+            Toast.makeText(requireContext(), "Share Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.aboutUsMenu) {
+            Toast.makeText(requireContext(), "About Us Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.inviteFriendMenu) {
+            Toast.makeText(requireContext(), "Invite Friend Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.rateUsMenu) {
+            Toast.makeText(requireContext(), "Rate Us Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.helpMenu) {
+            Toast.makeText(requireContext(), "Help and Feedback Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.logoutMenu) {
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(requireContext(), "Logout Successfully", Toast.LENGTH_SHORT).show();
+            Navigation.findNavController(getView()).navigate(R.id.action_homeFragment2_to_loginFragment);
+            return true;
+        }else
+            return false;
+    }
+
+    @Override
+    public void onSearchStateChanged(boolean enabled) {
+
+    }
+
+    @Override
+    public void onSearchConfirmed(CharSequence text) {
+
+    }
+
+    @Override
+    public void onButtonClicked(int buttonCode) {
+
     }
 }
+
+
+
+/*
+*  if (item.getItemId() == R.id.filter_menu) {
+            Navigation.findNavController(requireView()).navigate(R.id.action_homeFragment2_to_filterFragment);
+            return true;
+        } else if (item.getItemId() == R.id.shareMenu) {
+            Toast.makeText(requireContext(), "Share Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.aboutUsMenu) {
+            Toast.makeText(requireContext(), "About Us Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.inviteFriendMenu) {
+            Toast.makeText(requireContext(), "Invite Friend Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.rateUsMenu) {
+            Toast.makeText(requireContext(), "Rate Us Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.helpMenu) {
+            Toast.makeText(requireContext(), "Help and Feedback Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.logoutMenu) {
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(requireContext(), "Logout Successfully", Toast.LENGTH_SHORT).show();
+            Navigation.findNavController(getView()).navigate(R.id.action_homeFragment2_to_loginFragment);
+            return true;
+        }else
+            return false;*/
