004fee1ece0708cae67ae8873631db428e341841
JoelKanyi <joelkanyi98@gmail.com>
2021-03-17 09:02:36 +0300

    tried working on resetting filters

--- app/src/main/java/com/ifixhubke/kibu_olx/ui/fragments/FilterFragment.java
+++ app/src/main/java/com/ifixhubke/kibu_olx/ui/fragments/FilterFragment.java
@@ -4,15 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.navigation.NavDirections;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
-import com.google.android.material.slider.RangeSlider;
+import com.ifixhubke.kibu_olx.R;
 import com.ifixhubke.kibu_olx.data.Item;
 import com.ifixhubke.kibu_olx.databinding.FragmentFilterBinding;
 
@@ -20,7 +20,6 @@ import org.jetbrains.annotations.NotNull;
 
 import java.text.NumberFormat;
 import java.util.Currency;
-import java.util.List;
 
 import timber.log.Timber;
 
@@ -28,7 +27,7 @@ public class FilterFragment extends BottomSheetDialogFragment {
 
     FragmentFilterBinding binding;
     String category, condition;
-    double minPrice=0, maxPrice=10000;
+    double minPrice = 0, maxPrice = 10000;
 
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
@@ -36,16 +35,30 @@ public class FilterFragment extends BottomSheetDialogFragment {
         binding = FragmentFilterBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
-        Timber.d("Check on Slider default values: "+minPrice+" to: "+maxPrice);
+        Timber.d("Check on Slider default values: " + minPrice + " to: " + maxPrice);
 
-        binding.categoryChipGroup.setOnCheckedChangeListener((group, checkedId) -> category = getSelectedChipText(group, checkedId));
+        binding.categoryChipGroup.setOnCheckedChangeListener((group, checkedId) ->
+                category = getSelectedChipText(group, checkedId));
 
         binding.conditionChipGroup.setOnCheckedChangeListener((group, checkedId) -> condition = getSelectedChipText(group, checkedId));
 
         binding.textViewDone.setOnClickListener(v -> {
+
+            if (category == null && condition == null) {
+                Toast.makeText(requireContext(), "You must select an item category and its condition", Toast.LENGTH_SHORT).show();
+                return;
+            }
             Item item = new Item(category, condition, minPrice, maxPrice);
             NavDirections action = FilterFragmentDirections.actionFilterFragmentToFilteredItemsFragment(item);
             NavHostFragment.findNavController(this).navigate(action);
+
+        });
+
+        binding.textViewReset.setOnClickListener(v -> {
+            Chip chip = binding.categoryChipGroup.findViewById(R.id.chipElectronics);
+            chip.setChecked(true);
+            Chip chip2 = binding.conditionChipGroup.findViewById(R.id.chipNew);
+            chip2.setChecked(true);
         });
 
         binding.priceSlider.setLabelFormatter(value -> {
@@ -56,15 +69,12 @@ public class FilterFragment extends BottomSheetDialogFragment {
         });
 
 
-        binding.priceSlider.addOnChangeListener(new RangeSlider.OnChangeListener() {
-            @Override
-            public void onValueChange(@NonNull RangeSlider slider, float value, boolean fromUser) {
-                Timber.d("OnChangeListener");
-                minPrice = Double.parseDouble(Float.toString(slider.getValues().get(0)));
-                maxPrice = Double.parseDouble(Float.toString(slider.getValues().get(1)));
-                Timber.d("OnChangeListener From"+ minPrice);
-                Timber.d("OnChangeListener T0"+ maxPrice);
-            }
+        binding.priceSlider.addOnChangeListener((slider, value, fromUser) -> {
+            Timber.d("OnChangeListener");
+            minPrice = Double.parseDouble(Float.toString(slider.getValues().get(0)));
+            maxPrice = Double.parseDouble(Float.toString(slider.getValues().get(1)));
+            /*Timber.d("OnChangeListener From"+ minPrice);
+            Timber.d("OnChangeListener T0"+ maxPrice);*/
         });
 
         return view;
