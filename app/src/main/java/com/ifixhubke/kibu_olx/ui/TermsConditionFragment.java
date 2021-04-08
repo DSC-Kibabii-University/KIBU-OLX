6f6a4931104cca31417162335d9bc401c8f61577
JoelKanyi <joelkanyi98@gmail.com>
2021-04-08 10:14:28 +0300

    added fragment for terms and condition

---
+++ app/src/main/java/com/ifixhubke/kibu_olx/ui/TermsConditionFragment.java
@@ -0,0 +1,31 @@
+package com.ifixhubke.kibu_olx.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ifixhubke.kibu_olx.R;
+import com.ifixhubke.kibu_olx.databinding.FragmentTermsConditionBinding;
+
+public class TermsConditionFragment extends Fragment {
+
+    private FragmentTermsConditionBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentTermsConditionBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
+
+        binding.finished.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_termsConditionFragment_to_registerFragment);
+        });
+
+        return view;
+    }
+}
