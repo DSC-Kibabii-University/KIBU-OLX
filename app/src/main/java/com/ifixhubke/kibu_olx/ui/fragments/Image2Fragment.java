b40f17c5a22df176ae8d7216864270e656c4ae28
Collince-Okeyo <collinceokeyo98@gmail.com>
2021-04-09 23:43:08 +0300

    Added zooming of images

--- app/src/main/java/com/ifixhubke/kibu_olx/ui/fragments/Image2Fragment.java
+++ app/src/main/java/com/ifixhubke/kibu_olx/ui/fragments/Image2Fragment.java
@@ -32,16 +32,6 @@ public class Image2Fragment extends Fragment {
     FragmentImage2Binding binding;
     int col;
 
-    Matrix matrix = new Matrix();
-    Matrix savedMatrix = new Matrix();
-    PointF startPoint = new PointF();
-    PointF midPoint = new PointF();
-    float oldDist = 1f;
-    final int NONE = 0;
-    final int DRAG = 1;
-    final int ZOOM = 2;
-    int mode = NONE;
-
     public Image2Fragment(String imageUrl) {
         this.imageUrl = imageUrl;
     }
@@ -90,80 +80,6 @@ public class Image2Fragment extends Fragment {
                     }
                 })
                 .into(binding.imageViewImage2);
-
-
-
-
-       // ImageView imageDetail;
-
-
-      //  imageDetail = (ImageView) findViewById(R.id.imageView1);
-        view.setOnTouchListener(new View.OnTouchListener() {
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                //ImageView view = (ImageView) v;
-                //System.out.println("matrix=" + savedMatrix.toString());
-                switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                    case MotionEvent.ACTION_DOWN:
-
-                        savedMatrix.set(matrix);
-                        startPoint.set(event.getX(), event.getY());
-                        mode = DRAG;
-                        break;
-
-                    case MotionEvent.ACTION_POINTER_DOWN:
-
-                        oldDist = spacing(event);
-
-                        if (oldDist > 10f) {
-                            savedMatrix.set(matrix);
-                            midPoint(midPoint, event);
-                            mode = ZOOM;
-                        }
-                        break;
-
-                    case MotionEvent.ACTION_UP:
-
-                    case MotionEvent.ACTION_POINTER_UP:
-                        mode = NONE;
-
-                        break;
-
-                    case MotionEvent.ACTION_MOVE:
-                        if (mode == DRAG) {
-                            matrix.set(savedMatrix);
-                            matrix.postTranslate(event.getX() - startPoint.x, event.getY() - startPoint.y);
-                        } else if (mode == ZOOM) {
-                            float newDist = spacing(event);
-
-                            if (newDist > 10f) {
-                                matrix.set(savedMatrix);
-                                @SuppressLint("ClickableViewAccessibility") float scale = newDist / oldDist;
-                                matrix.postScale(scale, scale, midPoint.x, midPoint.y);
-                            }
-                        }
-                        break;
-                }
-                //view.setImageMatrix(matrix);
-
-                return true;
-            }
-
-            @SuppressLint("FloatMath")
-            private float spacing(MotionEvent event) {
-                float x = event.getX(0) - event.getX(1);
-                float y = event.getY(0) - event.getY(1);
-                return FloatMath.sqrt(x * x + y * y);
-            }
-
-            private void midPoint(PointF point, MotionEvent event) {
-                float x = event.getX(0) + event.getX(1);
-                float y = event.getY(0) + event.getY(1);
-                point.set(x / 2, y / 2);
-            }
-        });
 
 
 
